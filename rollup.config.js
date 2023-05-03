320387cf14702a1d97a59af4c52643868b6846b2
ymkim <ji9@me.com>
2023-05-03 14:35:31 +0900

    fix: 환경변수 업데이트

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ const packageJson = require('./package.json')
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
-    input: 'src/modules.ts',
+    input: 'modules.ts',
     output: [
       {
         file: packageJson.main,
