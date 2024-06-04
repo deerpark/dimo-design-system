b431f0bb452804f96cefbab3d3a73bd1d7b0261c
ymkim <ji9@me.com>
2024-06-04 16:09:50 +0900

    update: Button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -14,22 +14,22 @@ const getClasses = ({ size, variant, primary }: ButtonComponentStyleProps) => {
   /* const isGhost = variant === 'ghost' */
   const classesBySize = {
     mini: {
-      'px-1.5 py-0.5 text-xs rounded-md font-semibold': true,
+      'px-2 py-0.5 text-label4 rounded-md': true,
     },
     small: {
-      'px-2 py-1 text-sm rounded-md font-semibold': true,
+      'px-3 py-1.5 text-label3 rounded-md': true,
       'shadow-xs': isOutline,
     },
     medium: {
-      'px-4 py-1.5 text-base rounded-md font-semibold': true,
+      'px-[14px] py-2 text-label2 rounded-md': true,
       'shadow-sm': isOutline,
     },
     default: {
-      'px-5 py-2 text-base rounded-md font-bold': true,
+      'px-4 py-2.5 text-label2 rounded-md': true,
       'shadow-sm': isOutline,
     },
     large: {
-      'px-6 py-3 text-lg rounded-md font-bold': true,
+      'px-4 py-3 text-label1 rounded-md': true,
       'shadow-sm': isOutline,
     },
   }
@@ -68,13 +68,13 @@ function ButtonComponent({
   ...props
 }: ButtonComponentProps, ref: Ref<HTMLButtonElement>) {
   const classnames = cx(
+    getClasses({ size, variant, primary }),
     {
-      'relative inline-flex items-center justify-center outline-none transition-all whitespace-nowrap shrink-0': true,
+      'inline-flex items-center justify-center outline-none transition-all whitespace-nowrap shrink-0': true,
       'hover:opacity-70': true,
       'focus:ring-2 focus:ring-brand-primary focus:z-20': true,
       'disabled:text-content-disabled disabled:opacity-50 disabled:cursor-not-allowed': true,
     },
-    getClasses({ size, variant, primary }),
     {'text-brand-sunsetPink': badge},
     className,
   )
