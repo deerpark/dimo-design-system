3bb5647a3b21076d342e6e9b67689577620a84b3
ymkim <ji9@me.com>
2024-06-20 16:01:04 +0900

    chore: added rollup json plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
@@ -26,6 +27,7 @@ export default [
       external(),
       resolve(),
       commonjs(),
+      json(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
     ],
